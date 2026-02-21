91b0f9ec6b129b481f4c0e6af36235577ab60b7f
Rajukumar <rajuk631149@gmail.com>
2026-02-21 09:40:32 +0530

    Other important operator like type of

---
+++ Javascript-condition-loops-operators/Other Important Operators.js
@@ -0,0 +1,58 @@
+// typeof (Unary Operator): Returns a string indicating the type of an operand. code JavaScript
+
+// Peritive datatypes
+
+let user_name = "Rajukumar";
+console.log(user_name);
+console.log(typeof user_name);
+
+let Rollno = 34;
+console.log(Rollno);
+console.log(typeof Rollno);
+
+let salary_income = 85345.34;
+console.log(salary_income);
+console.log(typeof salary_income);
+
+let isMarrage = false;
+console.log(isMarrage);
+console.log(typeof isMarrage);
+
+let iscursh = true;
+console.log(iscursh);
+console.log(typeof iscursh);
+
+let moon_distance = 2354723212345678n;
+console.log(moon_distance);
+console.log(typeof moon_distance);
+
+let User_value = undefined;
+console.log(User_value);
+console.log(typeof User_value);
+
+let spacing = null;
+console.log(spacing);
+console.log(typeof spacing);
+
+let character = Symbol("asd");
+console.log(character);
+console.log(typeof character);
+
+//  Non - Permitive datatype
+let array = [10, 20, 30, 40];
+console.log(array);
+console.log(typeof array);
+
+const person_details = {
+  name: "Raju kumar",
+  Age: 22,
+  Phone: 345435345345324,
+};
+console.log(person_details);
+console.log(typeof person_details);
+
+function add(a, b) {
+  let sum = a + b;
+}
+console.log(add(3, 5));
+console.log(typeof add());
