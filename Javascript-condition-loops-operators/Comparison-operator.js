4382a556a15ecc3b0ee28dca117d9b4da41cc6d1
Rajukumar <rajuk631149@gmail.com>
2026-02-21 09:38:30 +0530

    Comparison operator  done

---
+++ Javascript-condition-loops-operators/Comparison-operator.js
@@ -0,0 +1,19 @@
+// Comparison operator 
+console.log(10 < 5 );
+console.log(10 > 5 );
+console.log(10 <= 5 );
+console.log(10 >= 5 );
+
+// (==) Loose Equality 
+console.log(10 == "10" );
+console.log(0 == false );
+
+// (===) Strict Equality 
+console.log(10 === "10");
+console.log(10 === 10);
+console.log(0 === false);
+
+// != (Loose Inequality) & !== (Strict Inequality): The same rules apply. Always prefer !==. 
+console.log("use different way like loose inequality and strict inequality");
+console.log(10 != "10");
+console.log(0 !== "false");
