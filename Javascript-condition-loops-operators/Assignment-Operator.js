c38e69ed46e317fdb6bf216a164480d894f0a5bd
Rajukumar <rajuk631149@gmail.com>
2026-02-21 09:37:53 +0530

    Assignment  operator  done

---
+++ Javascript-condition-loops-operators/Assignment-Operator.js
@@ -0,0 +1,31 @@
+// Assignment operator 
+let score = 100 ;
+console.log(score)
+let number_1 = 15;
+let number_2 = 3;
+console.log(number_1+=number_2);
+console.log(number_1-=number_2);
+console.log(number_1*=number_2);
+console.log(number_1/=number_2);
+console.log(number_1%=number_2);
+console.log(score**=number_2);
+
+//  basic example this is shorthand process 
+let x = 10 ; 
+let y  = 2;
+console.log(x += y)
+console.log(x -= y)
+console.log(x *= y)
+console.log(x /= y)
+console.log(x %= y)
+console.log(x **= y)
+
+//  now how people can write in mathematics 
+let   a = 12;
+let    b = 4;
+console.log(a = a + b);
+console.log(a = a - b);
+console.log(a = a * b);
+console.log(a = a / b);
+console.log(a = a % b);
+console.log(a = a ** b);
