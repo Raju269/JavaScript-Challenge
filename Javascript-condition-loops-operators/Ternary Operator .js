bcff0a229c356a726672628b0127e4d966c835e9
Rajukumar <rajuk631149@gmail.com>
2026-02-21 09:40:49 +0530

    Ternary Operator done

---
+++ Javascript-condition-loops-operators/Ternary Operator .js
@@ -0,0 +1,16 @@
+// - **Ternary Operator (? :):** A compact shorthand for an if...else statement. It's the only operator that takes three operands. code JavaScript
+//     - **Syntax:** condition ? expressionIfTrue : expressionIfFalse
+
+// let age = 12;
+// let message = (age>=18)?"You can vote ":"You cannot vote";
+// console.log(message);
+
+let a = 900;
+let b = 670;
+let greatestNumber =
+  a > b
+    ? console.log("A is  greatest Number b  ")
+    : console.log("B is greatest number");
+console.log(greatestNumber);
+
+// Ternary operator for multiple condition working case :
