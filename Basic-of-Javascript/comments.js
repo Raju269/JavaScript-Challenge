63bd8b35d73634df4248454af8f4bd9fb7d74b7a
Rajukumar <rajuk631149@gmail.com>
2026-02-22 19:19:51 +0530

    how to commit the code

---
+++ Basic-of-Javascript/comments.js
@@ -0,0 +1,11 @@
+// This is single line comment of javascript 
+console.log(`This is you 
+    not me you can understand 
+    better way like if you can help 
+    anyone they can help to you also.`)
+/*
+this is multiple comment 
+if we can write anything 
+in this comment it comment 
+the message
+*/
