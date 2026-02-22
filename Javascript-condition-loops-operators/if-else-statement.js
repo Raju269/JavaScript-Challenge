f923cecd84c23a8fea924d7cec4fec1c8e7b28fb
Rajukumar <rajuk631149@gmail.com>
2026-02-22 19:28:43 +0530

    if-else statement is done

---
+++ Javascript-condition-loops-operators/if-else-statement.js
@@ -0,0 +1,69 @@
+// if -else in js
+const wheather = 'raining';
+
+if(wheather){
+    console.log("Put on a raincoat. ")
+}
+else if(wheather){
+    console.log("Put on sunglassees. ")
+}
+else{
+    console.log("Just wear a regular sweater. ")
+}
+
+
+let temperature = 30;
+
+if (temperature > 25) {
+  console.log("It's a hot day! Wear shorts.");
+}
+
+// Code continues here...
+ 
+// if (condition) {
+  // This code runs if the condition is true.
+// } else {
+  // This code runs if the condition is false.
+// }
+
+
+let age = 16;
+
+if (age >= 18) {
+  console.log("You are old enough to vote.");
+} else {
+  console.log("You are not old enough to vote yet.");
+}
+
+
+
+// if (condition1) {
+  // Runs if condition1 is true.
+// } else if (condition2) {
+  // Runs if condition1 is false AND condition2 is true.
+// } else if (condition3) {
+  // Runs if 1 and 2 are false AND condition3 is true.
+// } else {
+  // Runs if ALL previous conditions were false.
+// }
+
+
+
+let score = 85;
+let grade;
+
+if (score >= 90) {
+  grade = 'A';
+} else if (score >= 80) {
+  grade = 'B';
+} else if (score >= 70) {
+  grade = 'C';
+} else if (score >= 60) {
+  grade = 'D';
+} else {
+  grade = 'F';
+}
+
+console.log(`Your grade is: ${grade}`);
+
+
